4a0af5ff8df7dae09ca79a6f4c3ca3df4845c989
David Michaels <dmichaels@gmail.com>
2025-08-04 18:18:18 -0400

    tec

--- Sandbox/ImageView.swift
+++ Sandbox/ImageView.swift
@@ -2,17 +2,27 @@ import SwiftUI
 
 public class ImageView
 {
-    private var _image: CGImage = DummyImage.instance
-    public var image: CGImage { self._image }
+    public private(set) var image: CGImage = DummyImage.instance
 
-    public func update(maxSize: CGSize, large: Bool = false) -> CGImage {
-        self._image = self.createImage(maxSize: maxSize, large: large)
-        return self._image
+    public func update(maxSize: CGSize, large: Bool = false, zoom: CGFloat? = nil) -> CGImage {
+        if let zoom: CGFloat = zoom {
+            var width: Int = Int(min(200 * zoom, maxSize.width))
+            var height: Int = Int(min(300 * zoom, maxSize.height))
+            self.image = self.createImage(width: width, height: height)
+        }
+        else {
+            self.image = self.createImage(maxSize: maxSize, large: large)
+        }
+        return self.image
     }
 
     private func createImage(maxSize: CGSize, large: Bool = false) -> CGImage /*?*/ {
         let width = !large ? 200 : Int(maxSize.width)
         let height = !large ? 300 : Int(maxSize.height)
+        return self.createImage(width: width, height: height)
+    }
+
+    private func createImage(width: Int, height: Int) -> CGImage /*?*/ {
         let context = CGContext(
             data: nil, width: width, height: height,
             bitsPerComponent: 8, bytesPerRow: width * 4, space: CGColorSpaceCreateDeviceRGB(),

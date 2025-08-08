361f8384f780580170f2851d64854f7ef3a1cb03
David Michaels <dmichaels@gmail.com>
2025-08-08 18:17:46 -0400

    delete-experimental-generic-toolbar-stuff-maybe-need-again-probably-not

--- Sandbox/SandboxApp.swift
+++ Sandbox/SandboxApp.swift
@@ -15,77 +15,7 @@ struct SandboxApp: App {
         }
     }
 
-/*
-    private let ToolBarViewLeading: ImageContentView.ToolBarViewMaker = { config in
-        ImageContentView.ToolBarViewable(
-            Text("Home")
-        )
-    }
-
-    private var ToolBarViewTrailing: ImageContentView.ToolBarViewMaker = { config in
-        ImageContentView.ToolBarViewable(
-            Button { config.showSettingsView() } label: { Image(systemName: "gearshape") }
-        )
-    }
-
-    func ToolBarView(config: ImageContentView.Config) -> [ImageContentView.ToolBarViewable] {
-        let ToolBarViewLeading: ImageContentView.ToolBarViewMaker = { config in
-            ImageContentView.ToolBarViewable(
-                Text("Home")
-            )
-        }
-        let ToolBarViewTrailing: ImageContentView.ToolBarViewMaker = { config in
-            ImageContentView.ToolBarViewable(
-                Button { config.showSettingsView() } label: { Image(systemName: "gearshape") }
-            )
-        }
-        return [
-            ToolBarViewLeading(config),
-            ToolBarViewTrailing(config)
-        ]
-    }
-*/
-
-/*
-    func ToolBarViewBuilders(config: ImageContentView.Config) -> [ImageContentView.ToolBarViewMaker] {
-        let x = SandboxApp.tb { _ in
-            Text("home")
-        }
-        let y = SandboxApp.tb { config in
-            Button { config.showSettingsView() } label: { Image(systemName: "gearshape") }
-        }
-let z = y(config)
-        return [
-            x,
-            y,
-            SandboxApp.tb { config in
-                Button { config.showSettingsView() } label: { Image(systemName: "gearshape") }
-            }
-        ]
-    }
-*/
-
     func ToolBarViews(config: ImageContentView.Config) -> [ImageContentView.ToolBarViewable] {
-/*
-        let x = ImageContentView.ToolBarViewBuilder { _ in
-            Text("hOMe")
-        }
-        let y = ImageContentView.ToolBarViewBuilder { config in
-            Button { config.showSettingsView() } label: { Image(systemName: "gearshape") }
-        }
-*/
-        // let xx = x(config)
-/*
-        return [
-            // xx,
-            ImageContentView.ToolBarViewBuilder { _ in
-                Text("hOMe")
-            }(config),
-            ImageContentView.ToolBarViewBuilder { config in
-                Button { config.showSettingsView() } label: { Image(systemName: "gearshape") }
-            }(config)
-        ]
-*/
         return [
             ImageContentView.ToolBarViewBuilder { _ in
                 Text("hOMe")
@@ -95,10 +25,4 @@ let z = y(config)
             }
         ].map { item in item(config) }
     }
-
-/*
-    public static func tb(@ViewBuilder _ make: @escaping (ImageContentView.Config) -> some View) -> (ImageContentView.Config) -> AnyView {
-        { cfg in AnyView(make(cfg)) }
-    }
-*/
 }

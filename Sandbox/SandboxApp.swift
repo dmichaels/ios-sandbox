0406233a18807ef3e247ba4bdd0df2d1130ac58a
David Michaels <dmichaels@gmail.com>
2025-08-09 07:50:57 -0400

    settings-view-generic-seems-to-work

--- Sandbox/SandboxApp.swift
+++ Sandbox/SandboxApp.swift
@@ -2,32 +2,10 @@ import SwiftUI
 
 @main
 struct SandboxApp: App {
-    private let config: ImageContentView.Config = ImageContentView.Config(ignoreSafeArea: false)
-    var body: some Scene {
-        WindowGroup {
-            ImageContentView(
-                config: self.config,
-                imageView: ImageView(self.config),
-                settingsView: SettingsView(self.config),
-                toolBarViews: ImageContentView.ToolBarView(self.config,
-                    ToolBarViewItems
-                /*
-                    ImageContentView.ToolBarItem { _ in
-                        Text("Home")
-                    },
-                    x,
-                    ImageContentView.ToolBarItem { config in
-                        Button { config.showSettingsView() } label: { Image(systemName: "gearshape") }
-                    }
-                    */
-                )
-            )
-        }
-    }
 
-    // FYI: Alternatively to above could defined toolbar items here like this.
-    //
-    let ToolBarViewItems: [ImageContentView.ToolBarItemBuilder] = [
+    private let config: ImageContentView.Config = ImageContentView.Config(ignoreSafeArea: false)
+
+    private let ToolBarViewItems: [ImageContentView.ToolBarItemBuilder] = [
         ImageContentView.ToolBarItem { _ in
             Text("Home")
         },
@@ -40,4 +18,15 @@ struct SandboxApp: App {
             }
         }
     ]
+
+    var body: some Scene {
+        WindowGroup {
+            ImageContentView(
+                config: self.config,
+                imageView: ImageView(self.config),
+                settingsView: SettingsView(self.config),
+                toolBarViews: ImageContentView.ToolBarView(self.config, ToolBarViewItems)
+            )
+        }
+    }
 }

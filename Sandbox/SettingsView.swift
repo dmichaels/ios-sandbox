3b779e86118857a94a9c7965c22e94bc19ac4790
David Michaels <dmichaels@gmail.com>
2025-08-09 11:24:36 -0400

    move-image-content-view-to-utils

--- Sandbox/SettingsView.swift
+++ Sandbox/SettingsView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Utils
 
 public struct SettingsView: ImageContentView.SettingsViewable {
 
--- Sandbox/ToolBarViews.swift
+++ Sandbox/ToolBarViews.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Utils
 
 public func ToolBarViews(_ config: ImageContentView.Config) -> ImageContentView.ToolBarViewables {
     return ImageContentView.ToolBarViewable(config, 

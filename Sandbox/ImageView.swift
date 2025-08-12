1b3b6860026da60e4e31f673547c3bbb790bf2ee
David Michaels <dmichaels@gmail.com>
2025-08-12 10:48:46 -0400

    etc

--- Sandbox/ImageView.swift
+++ Sandbox/ImageView.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 import Utils
+import CellGridView
 
 public class ImageView: ImageContentView.Viewable
 {
+    private var cellGridView: CellGridView = CellGridView()
     private var settings: Settings!
     public private(set) var image: CGImage = DummyImage.instance
-    private var viewSize: CGSize = CGSize.zero
+    private var viewWidth: Int = 0
+    private var viewHeight: Int = 0
     private var zoomStart: CGSize? = nil
 
     public init(settings: Settings) {
@@ -13,12 +16,28 @@ public class ImageView: ImageContentView.Viewable
     }
 
     public func update(viewSize: CGSize) {
-        self.image = self.createImage(viewSize: viewSize)
+        let viewWidth: Int = Int(viewSize.width)
+        let viewHeight: Int = Int(viewSize.height)
+        if ((viewWidth != self.viewWidth) || (viewHeight != self.viewHeight)) {
+            self.viewWidth = max(viewWidth, 1)
+            self.viewHeight = max(viewHeight, 1)
+            let imageSize = self.preferredImageSize(viewWidth: self.viewWidth, viewHeight: self.viewHeight)
+            self.image = self.createImage(width: imageSize.width, height: imageSize.height)
+        }
+
+        let cellSize: Int = 43
+        let preferredSize = CellGridView.preferredSize(cellSize: cellSize,
+                                                       viewWidth: self.viewWidth,
+                                                       viewHeight: self.viewHeight,
+                                                       fit: CellGridView.Fit.enabled,
+                                                       fitMarginMax: 100)
+        var x = 1
     }
 
     public func onTap(_ point: CGPoint) {
         self.settings.squareSizeSmall.toggle()
-        self.image = self.createImage(viewSize: self.viewSize)
+        let imageSize = self.preferredImageSize(viewWidth: self.viewWidth, viewHeight: self.viewHeight)
+        self.image = self.createImage(width: imageSize.width, height: imageSize.height)
         self.settings.contentView.updateImage()
     }
 
@@ -30,8 +49,8 @@ public class ImageView: ImageContentView.Viewable
 
     public func onZoom(_ zoomFactor: CGFloat) {
         if (self.zoomStart == nil) { self.zoomStart = CGSize(width: image.width, height: image.height) }
-        let width: Int = (CGFloat(self.zoomStart!.width) * zoomFactor).clampedInt(1.0...viewSize.width)
-        let height: Int = (CGFloat(self.zoomStart!.height) * zoomFactor).clampedInt(1.0...viewSize.height)
+        let width: Int = Int(CGFloat(self.zoomStart!.width) * zoomFactor).clamped(1...self.viewWidth)
+        let height: Int = Int(CGFloat(self.zoomStart!.height) * zoomFactor).clamped(1...self.viewHeight)
         self.image = self.createImage(width: width, height: height)
         self.settings.contentView.updateImage()
     }
@@ -45,17 +64,17 @@ public class ImageView: ImageContentView.Viewable
         self.settings.contentView.showSettingsView()
     }
 
-    private func createImage(viewSize: CGSize) -> CGImage {
-        self.viewSize = viewSize
-        let width = self.settings.squareSizeSmall ? 200 : Int(viewSize.width)
-        let height = self.settings.squareSizeSmall ? 300 : Int(viewSize.height)
-        return self.createImage(width: width, height: height)
+    private func preferredImageSize(viewWidth: Int, viewHeight: Int) -> (width: Int, height: Int) {
+        return (width:  (self.settings.squareSizeSmall ? 200 : Int(viewWidth)).clamped(1...viewWidth),
+                height: (self.settings.squareSizeSmall ? 300 : Int(viewHeight)).clamped(1...viewHeight))
     }
 
     private func createImage(width: Int? = nil, height: Int? = nil) -> CGImage {
         let width: Int = width ?? self.image.width
         let height: Int = height ?? self.image.height
-        guard width > 0, height > 0 else { return DummyImage.instance }
+        guard width > 0, height > 0 else {
+            return DummyImage.instance
+        }
         let context = CGContext(data: nil, width: width, height: height,
                                 bitsPerComponent: 8, bytesPerRow: width * 4, space: CGColorSpaceCreateDeviceRGB(),
                                 bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)!
@@ -68,6 +87,7 @@ public class ImageView: ImageContentView.Viewable
         let innerRectangleHeight: Int = height / 3
         context.fill(CGRect(x: (width - innerRectangleWidth) / 2, y: (height - innerRectangleHeight) / 2,
                             width: innerRectangleWidth, height: innerRectangleHeight))
-        return context.makeImage()!
+        let image: CGImage? = context.makeImage()
+        return image!
     }
 }
